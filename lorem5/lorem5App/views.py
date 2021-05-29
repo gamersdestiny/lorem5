09da14c3aac959b641815af86aa47ca8b60c392d
Gunarn6 <guna57012@gmail.com>
2021-05-29 21:38:06 +0530

    model vies

--- lorem5/lorem5App/views.py
+++ lorem5/lorem5App/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Contact
+from .models import Contacted
 # Create your views here.
 
 def home(request):
 	if request.method == 'POST':
-		contact = Contact()
+		contact = Contacted()
 		name = request.POST.get('Name')
 		email = request.POST.get('Email')
 		message = request.POST.get('Message')
