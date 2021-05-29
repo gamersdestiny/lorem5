7fcb411e13ff66b112cf760a3c92060e1aae4858
Gunarn6 <guna57012@gmail.com>
2021-05-29 12:23:57 +0530

    root conf changed static urls settings chamgdd

--- lorem5/lorem5/settings.py
+++ lorem5/lorem5/settings.py
@@ -116,13 +116,16 @@ USE_L10N = True
 
 USE_TZ = True
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 django_heroku.settings(locals())
