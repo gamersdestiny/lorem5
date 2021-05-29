d228a0c4af709bb084ce5c0309164190ec84d795
Gunarn6 <guna57012@gmail.com>
2021-05-29 11:51:38 +0530

    index style moved

--- lorem5/lorem5/urls.py
+++ lorem5/lorem5/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
-from lorem5.lorem5App import views as lorem5views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('lorem5.lorem5App.urls'), name='home' )
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
